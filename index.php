c00d2192ed35ae8812a874992e192a5bb6b351ab
TheFancyM00se <57185563+TheFanc7M00se@users.noreply.github.com>
2020-04-17 17:25:16 -0700

    Added all files from Repl.it portfolio

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Andres Silvera, per. 6]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <li><a href="Silvera_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Silvera_Projects.html">Projects.html</a></li></br>
+      <li><a href="Silvera_Resume.html">Resume.html</a></li></br>
+      <li><a href="Silvera_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html> 
